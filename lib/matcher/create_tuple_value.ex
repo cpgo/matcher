7d4bd48bbcf73979198fcb3b57e6b38f477e9def
Cassio Godinho <cassiopgodinho@gmail.com>
2020-09-22 17:47:39 -0300

    single rule

--- lib/matcher/create_tuple_value.ex
+++ lib/matcher/create_tuple_value.ex
@@ -1,6 +1,56 @@
 defmodule Matcher.CreateTupleValue do
   def generate_value(json) do
     {:ok, data} = Jason.decode(json, keys: :atoms)
-    data
+
+    rules =
+      data.rules.clauses
+      |> Enum.map(fn x -> extract_content(x) end)
+
+    wrap_content(rules)
   end
+
+  def extract_content(clauses) when not is_map_key(clauses, :logicalOperator) do
+    %{
+      lhs: clauses.content.key,
+      condition: clauses.content.condition,
+      rhs: clauses.content.value |> List.first()
+    }
+  end
+
+  def wrap_content(clause) do
+    {
+      %{
+        rules: clause
+      }
+    }
+  end
+
+
+  # this logic will be used for groups
+  # def extract_content(clauses = %{clauses: sub_clauses}) when length(sub_clauses) == 2 do
+  #   %{
+  #     lhs: extract_content(List.first(sub_clauses)),
+  #     operation: clauses.logicalOperator,
+  #     rhs: extract_content(List.last(sub_clauses))
+  #   }
+  # end
+
+  # def extract_content(clauses) when is_list(clauses) and length(clauses) == 2 do
+  #   [first, second | _] = clauses
+
+  #   %{
+  #     lhs: extract_content(first),
+  #     rhs: extract_content(second)
+  #   }
+  # end
+
+  # def extract_content(clauses) do
+  #   [first_clause | rest] = clauses.clauses
+
+  #   %{
+  #     lhs: extract_content(first_clause),
+  #     operation: clauses.logicalOperator,
+  #     rhs: extract_content(rest)
+  #   }
+  # end
 end
--- test/create_tuple_value_test.exs
+++ test/create_tuple_value_test.exs
@@ -27,11 +27,22 @@ defmodule EtsValueTest do
     }
     """
 
-    expected = {"username", "EQUAL", "user@email.com"}
+    expected = {
+      %{
+        rules: [
+          %{
+            lhs: "username",
+            condition: "EQUAL",
+            rhs: "user@email.com"
+          }
+        ]
+      }
+    }
+
     assert CreateTupleValue.generate_value(json_string) == expected
   end
 
-  test "parse multiple rule expression" do
+  test "parse two rule OR expression" do
     json_string = """
     {
       "name": "Composite",
@@ -72,21 +83,123 @@ defmodule EtsValueTest do
     }
     """
 
-    expected = {{"username", "EQUAL", "user@email.com"}, "OR", {"age", "GREATER_THAN", "18"}}
-    assert CreateTupleValue.rule_value(json_string) == expected
+    # expected = {{"username", "EQUAL", "user@email.com"}, "OR", {"age", "GREATER_THAN", "18"}}
+    expected = {
+      %{
+        operation: "OR",
+        rules: [
+          %{
+            lhs: "username",
+            condition: "EQUAL",
+            rhs: "email@email.com"
+          },
+          %{
+            lhs: "age",
+            condition: "GREATER_THAN",
+            rhs: "18"
+          }
+        ]
+      }
+    }
+
+    assert CreateTupleValue.generate_value(json_string) == expected
+  end
+
+  test "parse three rule OR expression" do
+    json_string = """
+    {
+    "name": "Composite",
+    "rules": {
+        "logicalOperator": "OR",
+        "type": "CLAUSE",
+        "clauses": [
+            {
+                "logicalOperator": "OR",
+                "type": "CLAUSE",
+                "clauses": [
+                    {
+                        "type": "RULE",
+                        "content": {
+                            "key": "username",
+                            "value": [
+                                "admin"
+                            ],
+                            "condition": "EQUAL"
+                        }
+                    },
+                    {
+                        "type": "RULE",
+                        "content": {
+                            "key": "age",
+                            "value": [
+                                "18"
+                            ],
+                            "condition": "GREATER_THAN"
+                        }
+                    },
+                    {
+                        "type": "RULE",
+                        "content": {
+                            "key": "role",
+                            "value": [
+                                "root"
+                            ],
+                            "condition": "EQUAL"
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    "authorId": "c7e6dafe-aa7a-4536-be1b-34eaad4c2915"
+    }
+    """
+
+    # expected = {
+    #   %{
+    #     operation: "OR",
+    #     lhs: %{
+    #       lhs: "username",
+    #       condition: "EQUAL",
+    #       rhs: "admin"
+    #     },
+    #     rhs: %{
+    #       lhs: %{
+    #         lhs: "age",
+    #         condition: "GREATER_THAN",
+    #         rhs: "18"
+    #       },
+    #       rhs: %{
+    #         lhs: "role",
+    #         condition: "EQUAL",
+    #         rhs: "root"
+    #       }
+    #     }
+    #   }
+    # }
+    expected = {
+      %{
+        operation: "OR",
+        clauses: [
+          %{
+            lhs: "username",
+            condition: "EQUAL",
+            rhs: "admin"
+          },
+          %{
+            lhs: "age",
+            condition: "GREATER_THAN",
+            rhs: "18"
+          },
+          %{
+            lhs: "role",
+            condition: "EQUAL",
+            rhs: "root"
+          }
+        ]
+      }
+    }
+
+    assert CreateTupleValue.generate_value(json_string) == expected
   end
 end
-
-[
-  {
-    {
-      {
-        :"$1",
-        {{"username", "user@email.com"}}
-      },
-      :"$2"
-    },
-    [],
-    [:"$1"]
-  }
-]
